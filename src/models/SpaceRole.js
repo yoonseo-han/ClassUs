71469508032bf9fa889527b4a6b7612e98104ee3
Yoonseo Han <yooncer00@gmail.com>
2023-07-16 18:51:25 +0800

    Added SpaceRole

---
+++ src/models/SpaceRole.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database');
+
+const SpaceRole = sequelize.define('SpaceRole', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+});
+
+module.exports = SpaceRole;
