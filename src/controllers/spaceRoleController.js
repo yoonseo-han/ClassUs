8ba96e1c86f7b87e9a2cfa1bdb9bf7b690c3055b
Yoonseo Han <yooncer00@gmail.com>
2023-08-08 00:14:49 +0800

    Function to assign space role to user based on status

--- src/controllers/spaceRoleController.js
+++ src/controllers/spaceRoleController.js
@@ -4,8 +4,30 @@ const sequelize = require('../../config/database');
 const spaceRoleController = {
     //Function to create new SpaceRole for a space
     assignSpaceRoleToUser : async(req, res) => {
-        //Get necessary data from the request body
-        const {userID, spaceID, } = req.body;
+        try {
+            //Get necessary data from the request body
+            const {userID, spaceID } = req.body;
+
+            //Assign role based on user id and user name
+            const user = await User.findByPk(userId);
+            const space = await Space.findByPk(spaceId);
+
+            //Error handling
+            if (!user || !space) {
+                throw new Error('User, Space, or SpaceRole not found.');
+            }
+
+            if(user.user_role == 'TA' || user.user_role == 'Professor') {
+                const roleId = 1;
+                await user.addSpace(space, { through: { roleId } });
+            } else {
+                const roleId = 2;
+                await user.addSpace(space, { through: { roleId } });
+            }
+            return true;
+        } catch (error) {
+            throw new Error('Failed to assign SpaceRole to User within Space.');
+        }
     },
 }
 
--- src/models/SpaceRole.js
+++ src/models/SpaceRole.js
@@ -2,6 +2,7 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../../config/database');
 
 const SpaceRole = sequelize.define('SpaceRole', {
+    //Define 1 => Manager, 2 => User
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
